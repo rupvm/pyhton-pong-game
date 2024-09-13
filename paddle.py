6db0c7fc076cde57fbc82eaa425da2d751174f7e
rupvm <halder.rupam2003@gmail.com>
2024-09-13 13:18:45 +0530

    first commit

---
+++ paddle.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+    def __init__(self,coordinates):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.penup()
+        self.goto(coordinates)
+
+
+
+
+    def move_up(self):
+        new_y = self.ycor() + 20
+        self.goto(x=self.xcor(), y=new_y)
+
+    def move_down(self):
+        new_y = self.ycor() - 20
+        self.goto(x=self.xcor(), y=new_y)
+
